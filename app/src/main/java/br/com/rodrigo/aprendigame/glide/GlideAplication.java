4bf03c4f4434a09e0617134721fd83d265581028
RodrigoBP99 <rodrigobozapontes@gmail.com>
2020-11-12 15:00:15 -0300

    Muda icones do aplicativo

---
+++ app/src/main/java/br/com/rodrigo/aprendigame/glide/GlideAplication.java
@@ -0,0 +1,8 @@
+package br.com.rodrigo.aprendigame.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class GlideApp extends AppGlideModule {
+}
